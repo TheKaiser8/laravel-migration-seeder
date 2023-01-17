9a277eb95c6341cd6653c81dba5c86c7048d4b08
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-17 17:14:57 +0100

    Aggiunge struttura tabella trains

--- database/migrations/2023_01_17_150328_create_trains_table.php
+++ database/migrations/2023_01_17_150328_create_trains_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', '50');
+            $table->string('departure_station', '100');
+            $table->string('arrival_station', '100');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', '10');
+            $table->unsignedTinyInteger('carriages_number');
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_deleted')->default(0);
+            $table->unsignedTinyInteger('railway');
             $table->timestamps();
         });
     }
