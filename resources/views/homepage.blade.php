56f4b303e8ba3b881ce458d29851d615e0427107
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-18 01:01:47 +0100

    Aggiunge PageController e rotta pagina homepage Crea views homepage e layout main Crea model Train

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('page-title')
+    Homepage
+@endsection
+
+@section('page-content')
+    <section>
+        <h1>Monitor arrivi e partenze treni</h1>
+@endsection
